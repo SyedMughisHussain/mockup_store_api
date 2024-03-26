b435ef22f58379f5b2ade756e120653d5fdfe939
SyedMuhgisHussain <syedmughis4541@gmail.com>
2024-03-26 18:38:48 +0500

    update and delete prouct query added

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -63,5 +63,44 @@ const createProduct = async (req, res) => {
   }
 };
 
+const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.find({ id: req.params.id }).select("-_id");
+    res.status(200).json({
+      status: "success",
+      message: "Product deleted successfully",
+      data: product,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
 
-export { getProducts, createProduct, getProduct };
+const updateProduct = async (req, res) => {
+  try {
+    const { title, price, description, image, category, rating } = req.body;
+    const product = await Product.find({ id: req.params.id }).select("-_id");
+    console.log(product[0].id);
+    product[0].title = title;
+    product[0].price = price;
+    product[0].description = description;
+    product[0].image = image;
+    product[0].category = category;
+    product[0].rating = rating;
+    res.status(200).json({
+      status: "success",
+      message: "Product updated successfully",
+      data: product,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
+
+export { getProducts, createProduct, getProduct, deleteProduct, updateProduct };
--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { getProducts, createProduct, getProduct } from "../controllers/product.controller.js"
+import { getProducts, createProduct, getProduct, deleteProduct, updateProduct } from "../controllers/product.controller.js"
 
 const router = express.Router();
 
 router.route('/').get(getProducts);
 router.route('/:id').get(getProduct);
+router.route('/:id').delete(deleteProduct);
+router.route('/:id').patch(updateProduct);
 router.route('/').post(createProduct);
 
 export default router
