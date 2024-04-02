bab5c02052396dd301894de74868f81cd099aab9
SyedMuhgisHussain <syedmughis4541@gmail.com>
2024-04-03 03:41:38 +0500

    Changes

--- server/index.js
+++ server/index.js
@@ -17,118 +17,13 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+// app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 app.get("/", (req, res) => {
   res.json({
     message: "Hello World!",
   });
 });
-app.get("/api/v1/hello", (req, res) => {
-    res.json({
-      message: "Hello World!",
-      data: [
-        {
-            name: "<NAME>",
-            email: "<EMAIL>",
-            password: "<PASSWORD>",
-            role: "admin",
-            address: "123 Main St, San Francisco, CA 94103, USA"
-        },
-        {
-            name: "<NAME>",
-            email: "<EMAIL>",
-            password: "<PASSWORD>",
-            role: "customer",
-            address: "123 Main St, San Francisco, CA 94103, USA"
-        },
-        {
-            name: "<NAME>",
-            email: "<EMAIL>",
-            password: "<PASSWORD>",
-            role: "customer",
-            address: "123 Main St, San Francisco, CA 94103, USA"
-        },
-        {
-            name: "<NAME>",
-            email: "<EMAIL>",
-            password: "<PASSWORD>",
-            role: "customer",
-            address: "123 Main St, San Francisco, CA 94103, USA"
-        },
-        {
-            name: "<NAME>",
-            email: "<EMAIL>",
-            password: "<PASSWORD>",
-            role: "customer",
-            address: "123 Main St, San Francisco, CA 94103, USA"
-        },
-        {
-            name: "<NAME>",
-            email: "<EMAIL>",
-            password: "<PASSWORD>",
-            role: "customer",
-            address: "123 Main St, San Francisco, CA 94103, USA"
-        },
-        {
-            name: "<NAME>",
-            email: "<EMAIL>",
-            password: "<PASSWORD>",
-            role: "customer",
-            address: "123 Main St, San Francisco, CA 94103, USA"
-        },
-        {
-            name: "<NAME>",
-            email: "<EMAIL>",
-            password: "<PASSWORD>",
-            role: "customer",
-            address: "123 Main St, San Francisco, CA 94103, USA"
-        },
-        {
-            name: "<NAME>",
-            email: "<EMAIL>",
-            password: "<PASSWORD>",
-            role: "customer",
-            address: "123 Main St, San Francisco, CA 94103, USA"
-        },
-        {
-            name: "<NAME>",
-            email: "<EMAIL>",
-            password: "<PASSWORD>",
-            role: "customer",
-            address: "123 Main St, San Francisco, CA 94103, USA"
-        },
-        {
-            name: "<NAME>",
-            email: "<EMAIL>",
-            password: "<PASSWORD>",
-            role: "customer",
-            address: "123 Main St, San Francisco, CA 94103, USA"
-        },
-        {
-            name: "<NAME>",
-            email: "<EMAIL>",
-            password: "<PASSWORD>",
-            role: "customer",
-            address: "123 Main St, San Francisco, CA 94103, USA"
-        },
-        {
-            name: "<NAME>",
-            email: "<EMAIL>",
-            password: "<PASSWORD>",
-            role: "customer",
-            address: "123 Main St, San Francisco, CA 94103, USA"
-        },
-        {
-            name: "<NAME>",
-            email: "<EMAIL>",
-            password: "<PASSWORD>",
-            role: "customer",
-            address: "123 Main St, San Francisco, CA 94103, USA"
-        }
-      ]
-    });
-  });
 app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/categories", categoriesRoutes);
 app.use("/api/v1/users", userRoutes);
