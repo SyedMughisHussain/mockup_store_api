f7535251736d8b641e4c6281fa5d514790609a2a
SyedMuhgisHussain <syedmughis4541@gmail.com>
2024-04-05 03:35:34 +0500

    Changes

--- client/src/components/Docs/Content.jsx
+++ client/src/components/Docs/Content.jsx
@@ -1,13 +1,8 @@
-import { Route, Routes } from "react-router-dom";
 
 const Content = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<div>About</div>} />
-        <Route path="/restApi" element={<div>Rest Api</div>} />
-        <Route path="/resources" element={<div>Resources</div>} />
-      </Routes>
+        <h1>Hello world</h1>
     </div>
   );
 };
