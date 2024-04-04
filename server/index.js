888f2e8d3d7dbc3241745ef491a6888b30d2bd05
SyedMuhgisHussain <syedmughis4541@gmail.com>
2024-04-04 05:34:21 +0500

    vercel config

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
-import Product from "./models/product.model.js";
 
 import productRoutes from "./routes/product.routes.js";
 import categoriesRoutes from "./routes/categories.routes.js";
@@ -16,9 +15,8 @@ dotenv.config({
 const app = express();
 
 
-app.use(express.json());
 app.use(cors());
-// app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
